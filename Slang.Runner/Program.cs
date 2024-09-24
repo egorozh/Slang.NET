25694c239d5b2fbb3e3c184c45b31cca2aa7293a
Егор Желудков <zheludkov@cmsplanet.ru>
2024-09-24 18:47:59 +0200

    fix: removed delay

--- Slang.Runner/Program.cs
+++ Slang.Runner/Program.cs
@@ -6,8 +6,6 @@ using Slang.Runner;
 
 #if DEBUG
 
-await Task.Delay(10000);
-
 Console.WriteLine("Start");
 
 var config = Test.GetConfig(args);
@@ -17,7 +15,6 @@ I18NBuilder builder = new(config);
 await builder.Build();
 
 Console.WriteLine("End");
-Console.ReadLine();
 
 #else
 var _ = BenchmarkRunner.Run<GenerateFiles>();
